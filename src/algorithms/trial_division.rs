cff99b2e7b75abede7deef7885bfa70823c3af8d
Caio Stoduto <88816833+caiostoduto@users.noreply.github.com>
2026-02-14 11:57:26 -0300

    feat: implement trial division algorithm

---
+++ src/algorithms/trial_division.rs
@@ -0,0 +1,13 @@
+pub fn is_prime(n: u64) -> bool {
+    if n <= 1 {
+        return false;
+    }
+
+    for i in 2..=((n as f64).sqrt() as u64) {
+        if n % i == 0 {
+            return false;
+        }
+    }
+
+    return true;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod algorithms;
+
+use algorithms::*;
+
 fn main() {
     println!("Hello, world!");
+    println!("Is 17 prime? {}", trial_division::is_prime(17));
 }
